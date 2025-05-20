90c2189879407b50acaaea13be3faffb26581935
Mohamed Zaid <moozaid86@gmail.com>
2025-05-20 16:08:10 +0300

    first commit2

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import postcss from './postcss.config.js';
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
-// postcss.config.js
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  css:{
+    postcss
+  }
+})
