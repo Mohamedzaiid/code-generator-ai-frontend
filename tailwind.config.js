eeb64e60d553cd6c468b9bab3a17b4fd6c2486c4
Mohamed Zaid <moozaid86@gmail.com>
2025-05-20 16:22:06 +0300

    first commit3

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,5 @@ export default {
   content: ["./index.html",'./src/**/*.{svelte,js,ts}'], // for unused CSS
   variants: {
     extend: {},
-  },
-  darkMode: false, // or 'media' or 'class'
+  }
 }
